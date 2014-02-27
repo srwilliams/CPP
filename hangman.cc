440b1e3f8d44c7bc9d8e79e71074b4a607e6ae9a
shelby <srwilliams444@gmail.com>
2014-02-27 13:49:26 -0600

    added words vector and file io

--- hangman.cc
+++ hangman.cc
@@ -6,9 +6,25 @@
 #include <cstring>
 #include <ctime>
 #include "hang.h"
+#include <vector>
 using namespace std;
 
 int main () {
+
+
+vector<string> words;
+int loop=0; //short for loop for input
+string line; //this will contain the data read from the file
+ifstream myfile ("words.txt");
+while(getline(myfile, line)) {
+	words.push_back(line);
+}
+
+
+//test for input
+/*for (unsigned int i=0; i<words.size(); ++i) {
+	cout<<words[i]<<endl;
+}*/
 
 
 cout<< "\n\t\tWelcome to Hangman!\n";
